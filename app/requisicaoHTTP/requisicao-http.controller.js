250018145774f815b9774c824a5d89ecd6adb006
GrandCharles <grandcharles@hotmail.com>
2016-12-02 00:06:21 -0200

    Versão 2.0

--- app/requisicaoHTTP/requisicao-http.controller.js
+++ app/requisicaoHTTP/requisicao-http.controller.js
@@ -1,12 +1,32 @@
 /**
- * Created by GrandCharles on 26/11/2016.
+ * Created by GrandCharles on 01/12/2016.
  */
-angular.module('pdApp').controller('RequisicaoHTTP',RequisicaoHTTP);
+(function () {
+    angular.module('pdApp').controller('HttpController', HttpController)
 
-function RequisicaoHTTP($http){
-    var vm = this;
-    vm.listaDados = [];
-    vm.carregaDados = carrega
+    function HttpController($http) {
+        var vm = this;
 
+        vm.listaDados = [];
 
-}
+        vm.gridOptions = {
+            data: 'vm.listaDados'
+        };
+
+        //não faça isso em produção
+        //coloque a chamada http no service
+        vm.carregarDados = carregarDados;
+        function carregarDados() {
+            $http.get('https://jsonplaceholder.typicode.com/photos').then(onPesquisarResult, onPesquisarFault);
+        }
+
+        function onPesquisarResult(response) {
+            vm.listaDados = response.data;
+        }
+
+        function onPesquisarFault(rejection) {
+
+        }
+    }
+
+})();
